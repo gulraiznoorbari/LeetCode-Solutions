01cd18a8bae01a809b945643b39d784b98dd5695
Gulraiz Noor Bari <gulraiznoorbari@gmail.com>
2021-10-13 00:13:06 +0500

    Adding Files

---
+++ BinarySearch/Medium/src/com/gulraiz/FirstandLastPositionofElement.java
@@ -0,0 +1,45 @@
+package com.gulraiz;
+
+import java.util.Arrays;
+
+public class FirstandLastPositionofElement {
+
+    public static void main(String[] args) {
+        // Question Link:
+        // https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+        int[] nums = {5,7,7,8,8,10};
+        int target = 7;
+        System.out.print(Arrays.toString(searchRange(nums, target)));
+    }
+    static int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1, -1};
+        // check for first occurrence if target is first:
+        ans[0] = search(nums, target, true);
+        if (ans[0] != -1) {
+            ans[1] = search(nums, target, false);
+        }
+        return ans;
+    }
+    static int search(int[] nums, int target, boolean findStartIndex) {
+        int ans = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < nums[mid]) {
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                // potential ans found:
+                ans = mid;
+                if (findStartIndex) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+}
