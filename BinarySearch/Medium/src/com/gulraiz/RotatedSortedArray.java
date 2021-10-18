6bbe6815136d0943d0735e70771aae9e7ba4080e
Gulraiz Noor Bari <gulraiznoorbari@gmail.com>
2021-10-18 14:17:37 +0500

    Adding Files

--- BinarySearch/Medium/src/com/gulraiz/RotatedSortedArray.java
+++ BinarySearch/Medium/src/com/gulraiz/RotatedSortedArray.java
@@ -41,6 +41,28 @@ public class RotatedSortedArray {
         return -1;
     }
 
+    // this will not work in duplicate values
+    static int findPivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            // 4 cases over here
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid-1;
+            }
+            if (arr[mid] <= arr[start]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
     static int findPivotWithDuplicate(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
