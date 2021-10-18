0df3a4aff93dc0e7b3a63389902eb09e427ea578
Gulraiz Noor Bari <gulraiznoorbari@gmail.com>
2021-10-18 14:23:54 +0500

    Adding Files

--- BinarySearch/Easy/src/com/gulraiz/RotationCount.java
+++ BinarySearch/Easy/src/com/gulraiz/RotationCount.java
@@ -4,8 +4,33 @@ public class RotationCount {
     // Question Link:
     // https://www.geeksforgeeks.org/find-rotation-count-rotated-sorted-array/
     public static void main(String[] args) {
-
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        System.out.print(countRotations(nums));
     }
 
+    static int countRotations(int[] arr) {
+        int pivot = findPivot(arr);
+        return pivot + 1;
+    }
 
+    static int findPivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            // 4 cases over here
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid-1;
+            }
+            if (arr[mid] <= arr[start]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
 }
